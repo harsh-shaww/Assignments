c23ed4f114abee8d0d8a8a7b89661f1068a2d6f9
twoplusfive <surendraprasadshaw8@gmail.com>
2023-01-24 17:30:42 +0530

    Create ques7.c

---
+++ Assignment3/ques7.c
@@ -0,0 +1,29 @@
+//7.Write a program to find out a number is prime or not.
+#include <stdio.h>
+
+int main() {
+    int num, i, flag = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (i = 2; i <= num/2; i++) {
+        if (num % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0)
+        printf("%d is a prime number.\n", num);
+    else
+        printf("%d is not a prime number.\n", num);
+
+    return 0;
+}
+
+/*
+Output:
+Enter a number: 5
+5 is a prime number.
+*/
