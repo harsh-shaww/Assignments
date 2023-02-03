c7c58794e489f6abfcf2a7996b9a9768c65f4467
twoplusfive <123461095+harsh-shaww@users.noreply.github.com>
2023-02-03 21:51:56 +0530

    Assignment 4

--- Assignment4/ques10.c
+++ Assignment4/ques10.c
@@ -2,36 +2,25 @@
 
 #include <stdio.h>
 
-int getFactorial(int N){
-    if(N < 0){
-        printf("Invalid Input: factorial not defined for negative numbers\n");
-        return 0;
-    }
-    int nFactorial = 1, counter;
+void printPascalTriangle(int n) {
+  int C[n][n];
 
-    for(counter = 1; counter <= N; counter++){
-        nFactorial = nFactorial * counter;
-    }    
-    return nFactorial;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j <= i; j++) {
+      if (j == 0 || j == i) {
+        C[i][j] = 1;
+      } else {
+        C[i][j] = C[i-1][j-1] + C[i-1][j];
+      }
+      printf("%d ", C[i][j]);
+    }
+    printf("\n");
+  }
 }
 
-int main() {  
-    int row, rows, i, value;
-   
-    printf("Enter Number of Rows of Pascal Triangle\n");  
-    scanf("%d", &rows);  
-   
-    for(row = 0; row < rows; row++) {   
-        for(i = row; i <= rows; i++)  
-            printf("  ");  
-   
-        for(i = 0; i <= row; i++) {  
-            value = getFactorial(row)/(getFactorial(i)*getFactorial(row-i));  
-            printf("%4d", value);  
-        }  
-   
-        printf("\n");  
-    }  
-   
-    return 0;  
-}  
+int main() {
+  int n = 5;
+  printPascalTriangle(n);
+  return 0;
+}
+ 
